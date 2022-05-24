c1b09006ddfa16628c4cbb61d4f659caf44ac306
XYPOJAI <jaijai.kc@gmail.com>
2022-05-24 14:38:19 +1000

    Update schema

---
+++ dist/config/dbCon.js
@@ -0,0 +1,17 @@
+"use strict";
+{
+    const mongoose = require("mongoose");
+    const connectDB = async () => {
+        try {
+            await mongoose.connect(process.env.DATABASE_URI, {
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
+    };
+    module.exports = connectDB;
+}
+//# sourceMappingURL=dbCon.js.map
---
+++ src/config/profileSchema.ts
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const ProfileSchema = new Schema({
+  id: { type: String },
+  firstName: { type: String },
+  lastName: { type: String },
+  phone: { type: String },
+  department: { type: String },
+  address: {
+    street: { type: String },
+    city: { type: String },
+    state: { type: String },
+    ZIP: { type: String },
+    country: { type: String },
+  },
+  created: { type: Date },
+  updated: { type: Date },
+});
+
+const Profile = model("Profile", ProfileSchema);
+
+module.exports = Profile;
